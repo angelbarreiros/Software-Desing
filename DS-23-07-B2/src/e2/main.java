2233a190a2893f349222dd35dc196bb1e99101db
angelbarreiros <angel.barreiros@udc.es>
2021-10-28 12:40:45 +0200

    Probando tests

--- DS-23-07-B2/src/e2/main.java
+++ DS-23-07-B2/src/e2/main.java
@@ -4,15 +4,17 @@ import java.util.Collections;
 
 public class main {
     public static void main(String[] args) {
-        ComparePostalCode code = new ComparePostalCode();
+        CompareTipo code = new CompareTipo();
+        ComparatorLocation postalCode=new ComparatorLocation();
         Idealista xd =new Idealista();
-        Vivienda casa= new Vivienda(Vivienda.TIPO.COMPRAR,1231321,4,200,300,15446,90,"A coruña");
-        Vivienda adosado= new Vivienda(Vivienda.TIPO.COMPRAR,1231321,4,200,300,15445,90,"A coruña");
-        Vivienda piso= new Vivienda(Vivienda.TIPO.COMPRAR,1231321,4,200,300,15444,90,"A coruña");
+        Vivienda casa= new Vivienda(Vivienda.TIPO.COMPRAR,1231321,4,200,300,15446,90,"Boruña");
+        Vivienda adosado= new Vivienda(Vivienda.TIPO.ALQUILAR,1231321,4,200,300,15445,90,"Aoruña");
+        Vivienda piso= new Vivienda(Vivienda.TIPO.COMPARTIR,1231321,4,200,300,15444,90,"Coruña");
         xd.add(casa);
         xd.add(adosado);
         xd.add(piso);
-        Collections.sort(xd.Lista,code);
+        Collections.sort(xd.Lista,postalCode);
+
         System.out.println(xd);
     }
 }
