a62deec801dedf98ab80167f719a0d681379a3e8
angelbarreiros <angel.barreiros@udc.es>
2021-12-01 13:09:42 +0100

    test 1 funciona

--- DS-23-07-PD/src/e2/API.java
+++ DS-23-07-PD/src/e2/API.java
@@ -1,4 +1,16 @@
 package e2;
 
 public class API {
+    private Dependency dependency;
+    private Graph graph=null;
+
+    public API(Graph graph) {
+        this.graph = graph;
+    }
+    public void setDependency(Dependency dependency) {
+        this.dependency = dependency;
+    }
+    public void work(){
+        dependency.ejecutar();
+    }
 }
