ea0cfcec23fbeec106408b6aed6fe227635b2160
angelbarreiros <angel.barreiros@udc.es>
2021-12-03 17:42:14 +0100

    todo terminado (preparar para hacer el documento)

--- DS-23-07-PD/test/e2/APITest.java
+++ DS-23-07-PD/test/e2/APITest.java
@@ -9,10 +9,17 @@ public class APITest {
     @Test
     public void Test() throws IOException {
         API api = new API();
-        Graph graph=api.graphMaker(api.fileReader("src/e2/xDependencies.txt"));
+        Graph graph=api.graphMaker(api.fileReader("src/e2/Prueba1Dependencies.txt"));
         Dependency_Weak dependency_weak= new Dependency_Weak();
         Dependency_Strong dependency_strong= new Dependency_Strong();
         Dependency_Hierarchy dependency_hierarchy=new Dependency_Hierarchy();
+        api.setDependency(dependency_strong);
+        api.work(graph);
+        api.setDependency(dependency_hierarchy);
+        api.work(graph);
+        api.setDependency(dependency_weak);
+        api.work(graph);
+        graph=api.graphMaker(api.fileReader("src/e2/Prueba2Dependencies.txt"));
         api.setDependency(dependency_strong);
         api.work(graph);
         api.setDependency(dependency_hierarchy);
